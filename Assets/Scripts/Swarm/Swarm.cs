5e9d3f3b1afafd37d94e6f049e79f4442fabc55a
Fabian Schober <schoberf@gmx.net>
2020-04-25 00:54:23 +0200

    Better Controls?

--- Assets/Scripts/Swarm/Swarm.cs
+++ Assets/Scripts/Swarm/Swarm.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using UnityEngine;
 
 public class Swarm : MonoBehaviour
@@ -13,6 +15,7 @@ public class Swarm : MonoBehaviour
     [SerializeField] private Vector2 velocity;
 
     private Rigidbody2D _rigidbody2D;
+    private TweenerCore<Quaternion, Quaternion, NoOptions> _rotationTweener;
 
     // Start is called before the first frame update
     void Start()
@@ -28,16 +31,23 @@ public class Swarm : MonoBehaviour
 
         var movementDirection = Vector2.ClampMagnitude(new Vector2(
             Mathf.Abs(Mathf.Pow(x, controlSharpness)) * Mathf.Sign(x),
-            Mathf.Abs(Mathf.Pow(y, controlSharpness)) * Mathf.Sign(y)),1);
+            Mathf.Abs(Mathf.Pow(y, controlSharpness)) * Mathf.Sign(y)), 1);
         _rigidbody2D.velocity = movementDirection * speed;
 
-        if (_rigidbody2D.velocity.sqrMagnitude > 0.1f)
+        if (new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).sqrMagnitude > 0.1f)
         {
             float angle = Mathf.Atan2(movementDirection.y, movementDirection.x) * Mathf.Rad2Deg - 90f;
             var rot = Quaternion.AngleAxis(angle, Vector3.forward);
-            
-            // transform.rotation = rot;
-            transform.DORotateQuaternion(rot, rotationDuration);
+
+            //transform.rotation = rot;
+            // if (_rotationTweener == null)
+            // {
+            _rotationTweener = transform.DORotateQuaternion(rot, rotationDuration);
+            // }
+            // else
+            // {
+            //     _rotationTweener = _rotationTweener.ChangeEndValue(rot, rotationDuration);
+            // }
         }
     }
 }
