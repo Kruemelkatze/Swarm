508206a5dbbb5eac78e8106ae0a2157b8a117a6b
Fabian Schober <schoberf@gmx.net>
2020-04-25 18:38:01 +0200

    Fish Movement Test

--- Assets/Scripts/Swarm/Fish.cs
+++ Assets/Scripts/Swarm/Fish.cs
@@ -6,15 +6,13 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
 
-    private SpriteRenderer _spriteRenderer;
-    [SerializeField] private Vector3 targetPosition;
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Vector3 relativeTargetPosition;
+    [SerializeField] private float movementSpeed = 3;
+    [SerializeField] private float rotationSpeed = 5;
+
+    public int index;
     
-
-    private void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
     // Start is called before the first frame update
     void Start()
     {
@@ -23,11 +21,23 @@ public class Fish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // var diff = relativeTargetPosition - transform.localPosition;
+        // diff.Normalize();
+        // float rotZ = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        // var targetRotation = Quaternion.Euler(0f, 0f, rotZ - 90);
+        var targetPosition = 
+            targetTransform.position +
+            targetTransform.rotation * relativeTargetPosition; 
+        //Vector3.Lerp(targetPosition, transform.position, movementSpeed * Time.deltaTime);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, TargetPosition.transform.rotation, rotationSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * movementSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetTransform.rotation, Time.deltaTime * 100);
+        
     }
-
+    
     public void SetColor(Color c)
     {
-        _spriteRenderer.color = c;
+        GetComponent<SpriteRenderer>().color = c;
     }
 
     public void SimplyRemove()
@@ -36,8 +46,14 @@ public class Fish : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public void SetTargetPosition(Vector3 transformPosition)
+    public void SetTargetPosition(Transform targetTransform, Vector3 relativeTargetPosition)
     {
-        targetPosition = targetPosition;
+        this.targetTransform = targetTransform;
+        this.relativeTargetPosition = relativeTargetPosition;
+    }
+
+    public void Eaten()
+    {
+        SimplyRemove();
     }
 }
